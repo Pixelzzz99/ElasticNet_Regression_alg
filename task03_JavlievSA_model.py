d1b39bf3d713aedd43378e2247a76fa2a85412be
Шерзод <Sherzik99@gmail.com>
2021-04-18 02:06:11 +0500

    Precise model

--- task03_JavlievSA_model.py
+++ task03_JavlievSA_model.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 
+
 class ElasticNetRegressor(object):
     def __init__(self, n_epoch=100, batch_size=100, alpha=100., delta=100.0, l1_coef=100.0, l2_coef=100.0):
         self.n_epoch = n_epoch
@@ -11,37 +12,47 @@ class ElasticNetRegressor(object):
         self.l2_coef = l2_coef
 
     def fit(self, X, Y):
-        self.m, self.n = X.shape
-        self.weights = np.zeros(self.n)
-        self.b = 0
         self.X = X
         self.Y = Y
-        for i in range(self.n_epoch):
-            self.update_weights()
+        batches_for_x = ElasticNetIterator(X, self.batch_size)
+        batches_for_y = ElasticNetIterator(Y, self.batch_size)
+        self.m, self.n = X.shape
+        
+        self.weights = np.zeros(self.n)
+        self.b = 0
+        
+        for epoch in range(self.n_epoch):
+            for batch_x, batch_y in zip(batches_for_x, batches_for_y):
+                self.update_weights(batch_x, batch_y)
         return self
 
-    def update_weights(self):
-        Y_pred = self.predict(self.X)
-        dW = np.zeros(self.n)
-        for j in range(self.n):
+    def update_weights(self, batch_x, batch_y):
+        x = np.array(batch_x)
+        y = np.array(batch_y)
+        m, n = np.shape(x)
+        Y_pred = self.predict(x)
+        dW = np.zeros(n)
+
+        for j in range(n):
             if(self.weights[j] > 0):
                 dW[j] = self._weight_bigger(j, Y_pred)
             else:
                 dW[j] = self._weight_less(j, Y_pred)
 
-        db = - 2 * np.sum(self.Y - Y_pred) / self.m
+        db = - 2 * np.sum(y - Y_pred) / m
         self.weights = self.weights - self.alpha * dW
         self.b = self.b - self.alpha * db
+        self.alpha = self.alpha / self.delta
         return self
 
     def _weight_bigger(self, index, y_pred):
-        dW_temp = (-(2 * (self.X[:, index]).dot(self.Y - y_pred)) +
-            self.l1_coef + 2 * self.l2_coef * self.weights[index])/self.m
+        dW_temp = (-(2 * (x[:, index]).dot(y - y_pred)) +
+                   self.l1_coef + 2 * self.l2_coef * self.weights[index])/m
         return dW_temp
 
     def _weight_less(self, index, y_pred):
-        dW_temp = (- (2 * (self.X[:, index]).dot(self.Y - y_pred)) -
-           self.l1_coef + 2 * self.l2_coef * self.weights[index])/self.m
+        dW_temp = (- (2 * (x[:, index]).dot(y - y_pred)) -
+                   self.l1_coef + 2 * self.l2_coef * self.weights[index])/m
         return dW_temp
 
     def predict(self, X):
@@ -52,16 +63,24 @@ class ElasticNetRegressor(object):
         v = ((y_gt - y_gt.mean())**2).sum()
         return (1-u/v)
 
-    class ElasticNetIterator:
-        def __init__(self, limit):
-            self.limit = limit
-            self.counter = 0
 
-        def __new__(self):
-            return self
-        
-        def __next__(self):
-            if self.counter < self.limit:
-                self.counter += 1
-                return 1
-            else: raise StopIteration
+class ElasticNetIterator:
+    def __init__(self, limit, batch_size):
+        self.data = np.array(limit)
+        self.batch_size = batch_size
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        starter = 0
+        finisher = self.batch_size
+        pointer = self.batch_size**2
+        if pointer < np.size(self.data):
+            batch = self.data[starter:finisher:, starter:finisher:].copy()
+            starter += finisher
+            finisher += self.batch_size
+            pointer += self.batch_size**2
+            return batch
+        else:
+            raise StopIteration
---
+++ task03_JavlievSA_tests.py
@@ -0,0 +1,42 @@
+import task03_JavlievSA_model
+import numpy as np
+from sklearn.model_selection import train_test_split 
+
+class UnitTestsElasticNet:
+    def __init__(self):
+        self.test = 0
+
+    def fit_function_test(self, X, Y):
+        return self
+
+    def update_weights_function_test(self):
+        return self
+
+    def weight_bigger_function_test(self):
+        return self
+
+    def weights_less_function_test(self):
+        return self
+
+    def predict_function_test(self):
+        return self
+
+    def score_function_test(self):
+        return self
+
+
+class ElasticNetRegressorsTester:
+    def __init__(self):
+        self.test = 0
+
+    def test1_simple_test(self):
+        return True
+
+    def test2_prefix1_test(self):
+        return True
+
+    def test3_prefix2_test(self):
+        return True
+
+    def test4_prefix4_test(self):
+        return True
