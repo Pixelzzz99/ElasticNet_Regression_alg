cc4c3643bfe534c857401650897411d4140df427
Шерзод <Sherzik99@gmail.com>
2021-04-20 18:25:15 +0500

    make my Model and add one test

--- task03_JavlievSA_model.py
+++ task03_JavlievSA_model.py
@@ -10,6 +10,8 @@ class ElasticNetRegressor(object):
         self.delta = delta
         self.l1_coef = l1_coef
         self.l2_coef = l2_coef
+        self.weights = 0
+        self.b = 0
 
     def fit(self, X, Y):
         self.X = X
@@ -17,43 +19,39 @@ class ElasticNetRegressor(object):
         batches_for_x = ElasticNetIterator(X, self.batch_size)
         batches_for_y = ElasticNetIterator(Y, self.batch_size)
         self.m, self.n = X.shape
-        
+
         self.weights = np.zeros(self.n)
-        self.b = 0
         
+
         for epoch in range(self.n_epoch):
             for batch_x, batch_y in zip(batches_for_x, batches_for_y):
                 self.update_weights(batch_x, batch_y)
         return self
 
     def update_weights(self, batch_x, batch_y):
-        x = np.array(batch_x)
-        y = np.array(batch_y)
-        m, n = np.shape(x)
-        Y_pred = self.predict(x)
-        dW = np.zeros(n)
+        
+        x_batch = np.array(batch_x)
+        y_batch = np.array(batch_y)
 
-        for j in range(n):
-            if(self.weights[j] > 0):
-                dW[j] = self._weight_bigger(j, Y_pred)
+        m_row, n_column = np.shape(x_batch)
+
+        Y_pred = self.predict(x_batch)
+
+        dW = np.zeros(self.n)
+
+        for j in range(n_column):
+            if self.weights[j] > 0:
+                dW[j] = (-(2 * x_batch[:, j].dot(y_batch - Y_pred)) +
+                         self.l1_coef + 2 * self.l2_coef * self.weights[j])/m_row
             else:
-                dW[j] = self._weight_less(j, Y_pred)
+                dW[j] = (- (2 * x_batch[:, j].dot(y_batch - Y_pred)) -
+                           self.l1_coef + 2 * self.l2_coef * self.weights[j])/m_row
 
-        db = - 2 * np.sum(y - Y_pred) / m
-        self.weights = self.weights - self.alpha * dW
-        self.b = self.b - self.alpha * db
+        db = - 2 * np.sum(y_batch - Y_pred) 
+        self.weights = self.weights - self.alpha * (dW / m_row)
+        self.b = self.b - self.alpha * (db / m_row)
         self.alpha = self.alpha / self.delta
         return self
-
-    def _weight_bigger(self, index, y_pred):
-        dW_temp = (-(2 * (x[:, index]).dot(y - y_pred)) +
-                   self.l1_coef + 2 * self.l2_coef * self.weights[index])/m
-        return dW_temp
-
-    def _weight_less(self, index, y_pred):
-        dW_temp = (- (2 * (x[:, index]).dot(y - y_pred)) -
-                   self.l1_coef + 2 * self.l2_coef * self.weights[index])/m
-        return dW_temp
 
     def predict(self, X):
         return X.dot(self.weights) + self.b
@@ -66,21 +64,33 @@ class ElasticNetRegressor(object):
 
 class ElasticNetIterator:
     def __init__(self, limit, batch_size):
-        self.data = np.array(limit)
+        self.data = limit.copy()
         self.batch_size = batch_size
+        self.starter = 0
+        self.finisher = self.batch_size
 
     def __iter__(self):
         return self
 
     def __next__(self):
-        starter = 0
-        finisher = self.batch_size
-        pointer = self.batch_size**2
-        if pointer < np.size(self.data):
-            batch = self.data[starter:finisher:, starter:finisher:].copy()
-            starter += finisher
-            finisher += self.batch_size
-            pointer += self.batch_size**2
+        if self.finisher < self.data.shape[0]:
+            batch = self.data[self.starter : self.finisher]
+            self.starter += self.batch_size
+            self.finisher += self.batch_size
             return batch
         else:
             raise StopIteration
+            
+
+
+'''
+    def _weight_bigger(self, x, y, m, index, y_pred):
+        dW_temp = (-(2 * (x[:, index]).dot(y - y_pred)) +
+           self.l1_coef + 2 * self.l2_coef * self.weights[index])/m
+        return dW_temp
+
+    def _weight_less(self, x, y, m, index, y_pred):
+        dW_temp = (- (2 * (x[:, index]).dot(y - y_pred)) -
+            self.l1_coef + 2 * self.l2_coef * self.weights[index])/m
+        return dW_temp
+'''
